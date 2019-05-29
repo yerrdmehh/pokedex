7766b4de99dde2ba80977a9323d7f6add0a7f8e7
Q. Price <priceqp@gmail.com>
2019-05-28 22:20:59 -0500

    Add Pokegame Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 //jshint esversion:6
 import React, { Component } from 'react';
-import Pokedex from './Pokedex';
+import Pokegame from './Pokegame';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
